c894e30b55eb0e9a81b714568a76927a35e54e63
Andy Tuttle <andytuttle12@gmail.com>
2016-12-20 17:28:17 -0500

    Added color, style and canvas elements

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,8 +8,51 @@ $(document).ready(function(){
 			console.log(weatherData);
 			var currentTemp = weatherData.main.temp;
 			var name = weatherData.name;
-			$('#currentTemp').html("The temp in " + name + " is currently " + currentTemp);
-			console.log(currentTemp);
+			var icon = weatherData.weather[0].icon + '.png';
+			$('#currentTemp').html("<img src='http://openweathermap.org/img/w/"+ icon +"'></img>The temp in " + name + " is currently " + currentTemp);
+			var canvas = $('#weather-canvas');
+			var context = canvas[0].getContext('2d');
+
+			var lineColor = 'black';
+			if(currentTemp < 32){
+				lineColor = '#AAF7FF';
+			}else if(currentTemp < 60){
+				lineColor = '#129793';
+			}else if(currentTemp < 70){
+				lineColor = '#80f470';
+			}else if(currentTemp < 80){
+				lineColor = '#ffc32d';
+			}else{
+				lineColor = '#f74f5d';
+			}
+			
+			var currentPercent = 0;
+			function animate(current){
+				context.clearRect(0,0,500, 500);
+				context.fillStyle = "#ccc";
+				context.beginPath();
+				context.arc(155,75,50,Math.PI * 0,Math.PI * 2);
+				context.closePath();
+				context.fill();
+
+				context.font = "40px Arial";
+				context.fillStyle = '#FFF';
+				context.fillText((Math.floor(currentTemp) + "\xB0F"),110,90);
+
+				context.lineWidth = 20;
+				context.strokeStyle = lineColor;
+				context.beginPath();
+				context.arc(155,75,60,Math.PI * 1.5,Math.PI * 2 * current + Math.PI * 1.5);
+				context.stroke();
+				currentPercent++;
+				if(currentPercent < currentTemp){
+					requestAnimationFrame(function(){
+						animate(currentPercent / 100);
+					});
+				}
+
+			}
+			animate();
 		});
 	});
 });
